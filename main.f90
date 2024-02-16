25a2c27913c2642f938b3518943143c97aff45f7
pepe <antonio.garcia.lattice@gmail.com>
2024-02-15 21:15:51 -0600

    linea 3 modificada por brandon

--- main.f90
+++ main.f90
@@ -5,7 +5,7 @@ program main
   print*, "Version 3"
   print*, "linea 1"
   print*, "linea 2"
-  print*, "linea 3"
+  print*, "linea 3. Modificada por Brandon."
   print*, "linea 4"
   print*, "linea 5"
 
